00621ea939a5441944ae12406cf80df90641f892
yao <yaozhj@gmail.com>
2024-04-14 16:10:33 -0400

    update

--- MethodExecutor.java
+++ MethodExecutor.java
@@ -1,42 +1,41 @@
 import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.Constructor;
-
 import java.io.InputStream;
 import java.lang.reflect.Method;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MethodExecutor {
-    public static void main(String[] args) throws Exception {
-        Yaml yaml = new Yaml(new Constructor(List.class));
-        InputStream inputStream = MethodExecutor.class.getClassLoader().getResourceAsStream("methods.yml");
-        if (inputStream == null) {
-            throw new RuntimeException("Cannot find 'methods.yml' in the classpath");
-        }
-        List<Map<String, Object>> data = yaml.load(inputStream);
+    public static void main(String[] args) {
+        try (InputStream inputStream = MethodExecutor.class.getClassLoader().getResourceAsStream("methods.yml")) {
+            if (inputStream == null) {
+                throw new RuntimeException("Cannot find 'methods.yml' in the classpath");
+            }
+            Yaml yaml = new Yaml();
+            Map<String, List<Map<String, Object>>> loadedYaml = yaml.load(inputStream);
+            List<Map<String, Object>> methods = loadedYaml.get("methods");  // Access the list by the "methods" key
 
-        ExecutorService executor = Executors.newFixedThreadPool(3); // Using 3 threads
-
-        for (Map<String, Object> item : data) {
-            executor.submit(() -> {
-                try {
-                    String methodName = (String) item.get("name");
-                    List<Object> params = (List<Object>) item.get("parameters");
-                    Class<?>[] paramTypes = new Class[params.size()];
-                    for (int i = 0; i < params.size(); i++) {
-                        paramTypes[i] = params.get(i).getClass();
+            ExecutorService executor = Executors.newFixedThreadPool(3); // Using 3 threads
+            for (Map<String, Object> item : methods) {
+                executor.submit(() -> {
+                    try {
+                        String methodName = (String) item.get("name");
+                        List<Object> params = (List<Object>) item.get("parameters");
+                        Class<?>[] paramTypes = new Class[params.size()];
+                        for (int i = 0; i < params.size(); i++) {
+                            paramTypes[i] = params.get(i).getClass();
+                        }
+                        Method method = MethodExecutor.class.getMethod(methodName, paramTypes);
+                        method.invoke(null, params.toArray());
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                    Method method = MethodExecutor.class.getMethod(methodName, paramTypes);
-                    method.invoke(null, params.toArray());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
+                });
+            }
+            executor.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        executor.shutdown();
     }
 
     public static void computeSum(Integer a, Integer b) {
