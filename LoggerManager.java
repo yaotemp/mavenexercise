4dec833822f8dbb93b1974679ec3fe177fab10e3
yao <yaozhj@gmail.com>
2024-04-25 20:46:50 -0400

    LoggerManager

--- LoggerManager.java
+++ LoggerManager.java
@@ -1,20 +1,23 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class LoggerManager {
-    static {
-        configureLogger("SystemLogger", "systemLogger.properties");
-        configureLogger("BusinessLogger", "businessLogger.properties");
-    }
 
     public static void configureLogger(String loggerName, String propertiesFileName) {
         Logger logger = Logger.getLogger(loggerName);
         try (FileInputStream fis = new FileInputStream(propertiesFileName)) {
             LogManager.getLogManager().readConfiguration(fis);
         } catch (IOException e) {
-            System.err.println("Could not load and apply properties file for " + loggerName + ": " + e.getMessage());
+            logger.log(Level.SEVERE, "Could not load and apply properties file for " + loggerName, e);
         }
         logger.setUseParentHandlers(false);
     }
 
-    public static Logger getLogger(String loggerName) {
+    public static Logger getLogger(String loggerName, String propertiesFileName) {
+        configureLogger(loggerName, propertiesFileName);
         return Logger.getLogger(loggerName);
     }
 }
